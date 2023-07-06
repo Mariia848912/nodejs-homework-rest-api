b7228bc1de62db6746f9c90e26d323dd1faa4c85
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-07-06 19:28:53 +0300

    use controllers

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const express = require("express");
-const logger = require("morgan");
+const logger = require("morgan"); // це middleware, яка виводить в консоль інформацію об запиті
 const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
 
-const app = express();
+const app = express(); // виклик функції express і є веб-сервіс
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());
-// ця middleware перевіряє чи є тіло в запиті.якщо є, то дивиться який по
-// заголовку Content-type, якщо json формат,  то робить express.json()
-app.use(express.json());
+app.use(cors()); // це для того, щоб express не відмовлял, якщо фронт и бєк на різних шляхах
 
-app.use("/api/contacts", contactsRouter);
-// обробка помилок коли прийшов запит на не існуючий адрес
+app.use(express.json()); // ця middleware перевіряє чи є тіло в запиті.якщо є, то дивиться який по
+// заголовку Content-type, якщо json формат,  то робить express.json()
+
+app.use("/api/contacts", contactsRouter); // всі маршрути які починаються з /api/contacts шукати тут
+
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
-});
-// інша обробка помилок
+}); // обробка помилок коли прийшов запит на не існуючий адрес
+
 app.use((err, req, res, next) => {
-  console.log(err);
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
+}); // інша обробка помилок
 
 module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,94 +1,17 @@
-const express = require("express");
-const contacts = require("../../models/contacts");
-const router = express.Router();
-const { HttpError } = require("../../helpers");
+const express = require("express"); // express створення веб сервіру (приймає http запити і відправляє відповіді)
+const router = express.Router(); // створюэ  одну "сторінку записної книжки ". створюємо шматочок веб сервісу
+const ctrl = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contacts");
 
-const Joi = require("joi");
-const schema = Joi.object({
-  name: Joi.string()
-    .required()
-    .messages({ "any.required": "Missing required name field" }),
-  email: Joi.string()
-    .required()
-    .messages({ "any.required": "Missing required email field" }),
-  phone: Joi.string()
-    .required()
-    .messages({ "any.required": "Missing required phone field" }),
-});
+router.get("/", ctrl.getAll);
 
-router.get("/", async (_, res, next) => {
-  try {
-    const allContacts = await contacts.listContacts();
-    res.json(allContacts);
-  } catch (error) {
-    next(error);
-    /*
-    інший варіант зловити одну помилку
-     res.status(500).json({ message: "Server error" });
-    */
-  }
-});
+router.get("/:contactId", ctrl.getContactById);
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const oneContact = await contacts.getContactById(contactId);
-    if (!oneContact) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(oneContact);
-  } catch (error) {
-    // коли в next передаэмо error, то express шукаэ обробник попилок.
-    // ця функція с 4 ма параметрами в файлі app.js
-    next(error);
-  }
-});
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = schema.validate(req.body);
+router.delete("/:contactId", ctrl.removeContact);
 
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
-    const newContacts = await contacts.addContact(req.body);
-    res.status(201).json(newContacts);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const removeContact = await contacts.removeContact(contactId);
-    if (!removeContact) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({ message: "Contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = schema.validate(req.body);
-
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { contactId } = req.params;
-
-    const updateContact = await contacts.updateContact(contactId, req.body);
-    if (!updateContact) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(updateContact);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateContact);
 
 module.exports = router;
