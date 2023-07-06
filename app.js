71e807257615629865eaf1e0c7bd14c1ec7238d8
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-07-06 13:58:21 +0300

    add code

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+// ця middleware перевіряє чи є тіло в запиті.якщо є, то дивиться який по
+// заголовку Content-type, якщо json формат,  то робить express.json()
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
-
+app.use("/api/contacts", contactsRouter);
+// обробка помилок коли прийшов запит на не існуючий адрес
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
-
+  res.status(404).json({ message: "Not found" });
+});
+// інша обробка помилок
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  console.log(err);
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
-module.exports = app
+module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,94 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts");
+const router = express.Router();
+const { HttpError } = require("../../helpers");
 
-const router = express.Router()
+const Joi = require("joi");
+const schema = Joi.object({
+  name: Joi.string()
+    .required()
+    .messages({ "any.required": "Missing required name field" }),
+  email: Joi.string()
+    .required()
+    .messages({ "any.required": "Missing required email field" }),
+  phone: Joi.string()
+    .required()
+    .messages({ "any.required": "Missing required phone field" }),
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_, res, next) => {
+  try {
+    const allContacts = await contacts.listContacts();
+    res.json(allContacts);
+  } catch (error) {
+    next(error);
+    /*
+    інший варіант зловити одну помилку
+     res.status(500).json({ message: "Server error" });
+    */
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const oneContact = await contacts.getContactById(contactId);
+    if (!oneContact) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(oneContact);
+  } catch (error) {
+    // коли в next передаэмо error, то express шукаэ обробник попилок.
+    // ця функція с 4 ма параметрами в файлі app.js
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = schema.validate(req.body);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (error) {
+      throw HttpError(400, error.message);
+    }
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    const newContacts = await contacts.addContact(req.body);
+    res.status(201).json(newContacts);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const removeContact = await contacts.removeContact(contactId);
+    if (!removeContact) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { contactId } = req.params;
+
+    const updateContact = await contacts.updateContact(contactId, req.body);
+    if (!updateContact) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(updateContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
