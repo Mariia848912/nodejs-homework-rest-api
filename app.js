f2059b6de786b0811ca9807e50f2ad2981d950b6
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-07-17 17:06:53 +0300

    add BD

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
-const logger = require("morgan"); // це middleware, яка виводить в консоль інформацію об запиті
+const logger = require("morgan"); // це middleware, яка виводить в консоль інформацію про запит
 const cors = require("cors");
-
+const dotenv = require("dotenv"); // require("dotenv").config(); - можливо так написати
+// читає файл env і додає дані до process.env (змінні оточення )
 const contactsRouter = require("./routes/api/contacts");
-
 const app = express(); // виклик функції express і є веб-сервіс
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
+dotenv.config(); // читає файл env і додає дані до process.env (змінні оточення )
 app.use(logger(formatsLogger));
 app.use(cors()); // це для того, щоб express не відмовлял, якщо фронт и бєк на різних шляхах
 
 app.use(express.json()); // ця middleware перевіряє чи є тіло в запиті.якщо є, то дивиться який по
 // заголовку Content-type, якщо json формат,  то робить express.json()
 
-app.use("/api/contacts", contactsRouter); // всі маршрути які починаються з /api/contacts шукати тут
-
+app.use("/api/contacts", contactsRouter);
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 }); // обробка помилок коли прийшов запит на не існуючий адрес
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,28 @@
 const express = require("express"); // express створення веб сервіру (приймає http запити і відправляє відповіді)
 const router = express.Router(); // створюэ  одну "сторінку записної книжки ". створюємо шматочок веб сервісу
 const ctrl = require("../../controllers/contacts");
-const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+const { validateBody, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", isValidId, ctrl.getContactById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:contactId", ctrl.removeContact);
+router.delete("/:contactId", isValidId, ctrl.removeContact);
 
-router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateContact);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateContact
+);
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
 module.exports = router;
